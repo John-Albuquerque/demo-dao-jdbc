e9243858c23b3c9448182cba2418408ed537e171
Jonathan Albuquerque <jonathanalbuquerquq@gmail.com>
2025-12-31 19:12:17 -0300

    delete implementation

--- src/application/Program.java
+++ src/application/Program.java
@@ -5,13 +5,15 @@ import models.dao.SellerDao;
 import models.entities.Department;
 import models.entities.Seller;
 
-import java.time.LocalDate;
 
+import java.time.LocalDate;
 import java.util.List;
+import java.util.Scanner;
 
 
 public class Program {
     public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
 
         SellerDao sellerDao = DaoFactory.createSellerDao();
 
@@ -30,19 +32,27 @@ public class Program {
         list = sellerDao.findAll();
         list.forEach(System.out::println);
 
-
         /*
         System.out.println("\n=== TEST 4: seller insert ====");
         Seller newSeller = new Seller(null, "Greg", "greg@dev.com", LocalDate.now(), 4040.00, department);
         sellerDao.insert(newSeller);
         System.out.println("Inserted! New id = " + newSeller.getId());
-
          */
+
+
 
         System.out.println("\n=== TEST 5: seller update ====");
         seller = sellerDao.findById(1);
         seller.setName("Marta Waine");
         sellerDao.update(seller);
         System.out.println("Update completed");
+
+        System.out.println("\n=== TEST 6: seller delete ====");
+        System.out.println("Enter id for delete test: ");
+        int id = sc.nextInt();
+        sellerDao.deletedById(id);
+        System.out.println("Delete completed");
+
+        sc.close();
     }
 }
